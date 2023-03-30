d23732f6ff773f69237b8c93a037ea101764dd28
ender7396 <lss933673961234@gmail.com>
2023-03-30 18:56:57 +0900

    랜덤숫자 맞추기

--- Program.cs
+++ Program.cs
@@ -6,9 +6,29 @@ namespace VS_CODE_CS
     {
         static void Main(string[] args)
         {
-            string[] str = Console.ReadLine().Split();
+            Random random = new Random();
+            int num = random.Next(1,101);   
+            Console.Write("1 ~ 100까지의 숫자를 입력해주세요 : ");
 
-            Console.WriteLine($"{str[0]} {str[1]}");
+            int input = 0;
+            while(input != num) 
+            {
+                input = int.Parse(Console.ReadLine());
+
+                if(input > num)
+                {
+                    Console.WriteLine("제 마음속의 수는 그것보다 작습니다.....");
+                }
+                else if(input < num)
+                {
+                    Console.WriteLine("제 마음속의 수는 그것보다 큽니다.......");
+                }
+                else
+                {
+                    Console.WriteLine("제 마음속의 수는 그것입니다!!");
+                }
+            }
+
         }
     }
 }
